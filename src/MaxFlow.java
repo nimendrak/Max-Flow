2334263c639ecbcd8202693e72abf17718632824
NimendraK <nimendra.2019264@iit.ac.lk>
2021-03-22 13:35:30 +0530

    parser added

--- src/MaxFlow.java
+++ src/MaxFlow.java
@@ -1,12 +1,10 @@
-// Java program for implementation of Ford Fulkerson
-// algorithm
+// Java program for implementation of Ford Fulkerson algorithm
 
-import java.io.*;
 import java.lang.*;
 import java.util.LinkedList;
 
 class MaxFlow {
-    static final int V = 6; // Number of vertices in graph
+    static int VERTICES; // Number of vertices in graph
 
     /* Returns true if there is a path from source 's' to
     sink 't' in residual graph. Also fills parent[] to
@@ -14,13 +12,14 @@ class MaxFlow {
     boolean bfs(int[][] rGraph, int s, int t, int[] parent) {
         // Create a visited array and mark all vertices as
         // not visited
-        boolean[] visited = new boolean[V];
-        for (int i = 0; i < V; ++i)
+        boolean[] visited = new boolean[VERTICES];
+        for (int i = 0; i < VERTICES; ++i)
             visited[i] = false;
 
         // Create a queue, enqueue source vertex and mark
         // source vertex as visited
-        LinkedList<Integer> queue = new LinkedList<>();
+        LinkedList<Integer> queue
+                = new LinkedList<Integer>();
         queue.add(s);
         visited[s] = true;
         parent[s] = -1;
@@ -29,7 +28,7 @@ class MaxFlow {
         while (queue.size() != 0) {
             int u = queue.poll();
 
-            for (int v = 0; v < V; v++) {
+            for (int v = 0; v < VERTICES; v++) {
                 if (!visited[v] && rGraph[u][v] > 0) {
                     // If we find a connection to the sink
                     // node, then there is no point in BFS
@@ -53,7 +52,8 @@ class MaxFlow {
 
     // Returns tne maximum flow from s to t in the given
     // graph
-    int fordFulkerson(int[][] graph, int s, int t) {
+    int fordFulkerson(int ver, int[][] graph, int s, int t) {
+        VERTICES = ver;
         int u, v;
 
         // Create a residual graph and fill the residual
@@ -64,16 +64,16 @@ class MaxFlow {
         // residual capacity of edge from i to j (if there
         // is an edge. If rGraph[i][j] is 0, then there is
         // not)
-        int[][] rGraph = new int[V][V];
+        int[][] rGraph = new int[VERTICES][VERTICES];
 
-        for (u = 0; u < V; u++) {
-            for (v = 0; v < V; v++) {
+        for (u = 0; u < VERTICES; u++) {
+            for (v = 0; v < VERTICES; v++) {
                 rGraph[u][v] = graph[u][v];
             }
         }
 
         // This array is filled by BFS and to store path
-        int[] parent = new int[V];
+        int[] parent = new int[VERTICES];
 
         int max_flow = 0; // There is no flow initially
 
@@ -105,4 +105,3 @@ class MaxFlow {
         return max_flow;
     }
 }
-
--- src/TestData.java
+++ src/TestData.java
@@ -1,37 +1,79 @@
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.io.*;
+import java.util.*;
 
 public class TestData {
+    static int VERTICES = 0;
+
     static List<String> bridgeArr = new ArrayList<>();
     static List<String> ladderArr = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         File folder = new File("src/SampleData");
         File[] listOfFiles = folder.listFiles();
 
         for (int i = 0; i < Objects.requireNonNull(listOfFiles).length; i++) {
             if (listOfFiles[i].isFile()) {
-                if (listOfFiles[i].getName().equalsIgnoreCase("ladder")) {
+                if (listOfFiles[i].getName().contains("ladder")) {
                     ladderArr.add(listOfFiles[i].getName());
                 }
-                if (listOfFiles[i].getName().equalsIgnoreCase("bridge")) {
+                if (listOfFiles[i].getName().contains("bridge")) {
                     bridgeArr.add(listOfFiles[i].getName());
                 }
             }
         }
 
-        // Let us create a graph shown in the above example
-        int[][] graph = new int[][]{
-                {0, 16, 13, 0, 0, 0}, {0, 0, 10, 12, 0, 0},
-                {0, 4, 0, 0, 14, 0}, {0, 0, 9, 0, 0, 20},
-                {0, 0, 0, 7, 0, 4}, {0, 0, 0, 0, 0, 0}
-        };
-        MaxFlow m = new MaxFlow();
+//        File f = new File("src/SampleData/" + bridgeArr.get(0));
 
-        System.out.println("The maximum possible flow is " + m.fordFulkerson(graph, 0, 5));
+        String fileName = "src/SampleData/bridge_1.txt";
+
+        // Number of ints per line:
+        int width = 3;
+
+        // This will be the output - a list of rows, each with 'width' entries:
+        ArrayList<int[]> dataArr = new ArrayList<>();
+
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            Scanner sc = new Scanner(bufferedReader);
+
+            VERTICES = Integer.parseInt(sc.nextLine());
+            System.out.println("VERTICES  -> " + VERTICES);
+
+            // While we've got another line..
+            while (sc.hasNextLine()) {
+                // Setup current row:
+                int[] row = new int[width];
+                // For each number..
+                for (int i = 0; i < width; i++) {
+                    // Read the number and add it to the current row:
+                    row[i] = sc.nextInt();
+                }
+                // Add the row to the dataArr:
+                dataArr.add(row);
+                // Go to the next line (optional, but helps deal with erroneous input files):
+                if (sc.hasNextLine()) {
+                    // Go to the next line:
+                    sc.nextLine();
+                }
+            }
+            sc.close();
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("Unable to open file: " + fileName);
+        }
+
+        int[][] data2dArr = new int[dataArr.size()][];
+        for (int i = 0; i < data2dArr.length; i++) {
+            int[] row = dataArr.get(i);
+            data2dArr[i] = row;
+        }
+
+        System.out.println("ARR SIZE  -> " + data2dArr.length);
+        System.out.println(Arrays.deepToString(data2dArr));
+
+        MaxFlow maxFlow = new MaxFlow();
+        System.out.println("The maximum possible flow is " + maxFlow.fordFulkerson(VERTICES, data2dArr, 0, (VERTICES - 1)));
     }
-
 }
